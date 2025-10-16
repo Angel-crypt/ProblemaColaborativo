d2d37dd6d89ebba63ffb1612a2e38b88f77b10f7
Angel-crypt <angel.cruz.030905@gmail.com>
2025-10-15 19:23:38 -0600

    feat: add BaseProduct

---
+++ src/BaseProduct.java
@@ -0,0 +1,31 @@
+public abstract class BaseProduct {
+    private final int id;
+    private final String name;
+    private final double price;
+    private final int amount;
+
+    public BaseProduct(int id, String name, double price, int amount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public abstract void showInfo();
+}
