72fbbc472e70bb7e2d37bdebac34f60da12d0c36
Angel-crypt <angel.cruz.030905@gmail.com>
2025-10-20 13:50:36 -0600

    feat: implement AppicableDiscount interface in BaseProduct and add setter methods for price and amount

--- src/model/base/BaseProduct.java
+++ src/model/base/BaseProduct.java
@@ -1,10 +1,10 @@
 package model.base;
 
-public abstract class BaseProduct {
+public abstract class BaseProduct implements AppicableDiscount{
     private final int id;
     private final String name;
-    private final double price;
-    private final int amount;
+    private double price;
+    private int amount;
 
     public BaseProduct(int id, String name, double price, int amount) {
         this.id = id;
@@ -24,10 +24,12 @@ public abstract class BaseProduct {
     public double getPrice() {
         return price;
     }
+    public void setPrice(double price) { this.price = price; }
 
     public int getAmount() {
         return amount;
     }
+    public void setAmount(int amount) { this.amount = amount; }
 
     public abstract void showInfo();
 }
