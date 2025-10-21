7a13922769929a46e2b93de5d0d1688b1ba560f6
abdy <abdyn.7@gmail.com>
2025-10-20 18:05:02 -0600

    agregar getters y setters para atributo type

--- src/model/products/Accesory.java
+++ src/model/products/Accesory.java
@@ -4,4 +4,25 @@ import model.base.BaseProduct;
 import model.base.AppicableDiscount;
 
 public class Accessory extends BaseProduct implements AppicableDiscount {
+
+    private static int countAccessory = 0;
+
+    private String type;
+
+    public Accessory(String name, double price, int amount, String type) {
+        super(generateId(type), name, price, amount);
+        this.type = type;
+    }
+
+    // Método para generar el ID automáticamente
+    private static String generateId(String type) {
+        String typeData = type.length() >= 2
+                ? type.substring(0, 2).toUpperCase()
+                : type.toUpperCase();
+
+        String counter = String.format("%03d", countAccessory);
+        countAccessory++;
+
+        return counter + "-AC-" + typeData;
+    }
 }
